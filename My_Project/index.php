53b0c8b7ca94bb39162d2172a6f46f67a6fdcab8
KenanK0 <79925521+KenanK0@users.noreply.github.com>
2021-05-24 16:07:29 +0200

    video on the home page

--- My_Project/index.php
+++ My_Project/index.php
@@ -42,6 +42,8 @@ include("connection.php");
   <div class="main1">
     <p class="mh" >MENTAL HEALTH</p>
     <div id = "mental_para">
+    <iframe width="520" height="315" src="https://www.youtube.com/embed/G0zJGDokyWQ?start=20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
     <p class="mhh" >Mental health is an integral and essential component of health. According to the WHO constitution, health is a state of complete physical, mental and social well-being and not merely the absence of disease or infirmity. An important implication of this definition is that mental health is more than just the absence of mental disorders or disabilities.</p>
     <p class="mhh">Mental health is a state of well-being in which an individual realizes his or her own abilities, can cope with the normal stresses of life, can work productively and is able to contribute to his or her community. Mental health is determined by a range of socioeconomic, biological and environmental factors.</p>
     </div>
